459ec43cbe735c9de14065800e051b56c6c02fdd
Ch4rm <benrahays@gmail.com>
2020-06-20 16:36:28 +0000

    init2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def init(agents, debug=False):
-
+  for agent in range(agents):
+    agents.create_agent()
+  if debug:
+    print("(main.py) initialize command successful")
+class agents():
+  def create_agent():
+   
